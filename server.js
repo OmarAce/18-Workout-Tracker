a84f533f5e415498636e568a8151ad24a7aaddf7
Ace <omarace95@gmail.com>
2022-02-05 20:33:03 -0800

    return to prev

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // connecting with MongoDB via mongoose at our myapp database running locally on default port 27017
-mongoose.connect( 'mongodb+srv://root:root@workouttracker.8evwl.mongodb.net/workout?retryWrites=true&w=majority' || "mongodb://localhost/workout", { 
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/workout", { 
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useFindAndModify: false 
